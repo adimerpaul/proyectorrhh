f556e4dab3a96aa808a4806c09886a9c5f5e242c
adimerpaul <adimer101@email.com>
2022-04-07 20:55:03 -0400

    crud terminado con la fotografia

--- back/app/Http/Controllers/PersonaController.php
+++ back/app/Http/Controllers/PersonaController.php
@@ -32,6 +32,17 @@ class PersonaController extends Controller
             return "no existe la imagen";
         }
     }
+    public function base64(Request $request){
+        if ($request->imagen==''){
+            return '';
+        }
+        $path = 'imagenes/'.$request->imagen;
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        return $base64;
+
+    }
 
     /**
      * Show the form for creating a new resource.
--- back/routes/api.php
+++ back/routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 //});
 Route::apiResource('/persona',\App\Http\Controllers\PersonaController::class);
 Route::post('/upload',[\App\Http\Controllers\PersonaController::class,'upload']);
+Route::post('/base64',[\App\Http\Controllers\PersonaController::class,'base64']);
